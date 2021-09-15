19d921be130099e57b49e76766591665bb37a5e4
Luciano Rodrigues <lamkrodrigues@gmail.com>
2021-09-15 14:17:08 -0300

    Update Double.dart

--- lib/src/core/Double.dart
+++ lib/src/core/Double.dart
@@ -1,11 +1,11 @@
 class Double
 {
-  static double parse(var value) {
+  static double tryParse(var value, double valueIfError) {
     if( value is int ) {
       return 0.0 + value;
     } else if( value is String ) {
       return double.parse(value);
     }
-    return value;
+    return valueIfError;
   }
 }
